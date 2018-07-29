26233411ea07071ef203ca2aafb4603a5c832160
komaljit-singh <komaljit.singh@openx.com>
2018-07-29 15:33:04 -0700

    adding the inital template

---
+++ rest_framework/router/router.py
@@ -0,0 +1,12 @@
+class RouterMiddleWare:
+
+    def __init__(self):
+        pass
+
+    def parse_url(self, request):
+        pass
+
+    def identify_request_method(self, request):
+        pass
+
+
